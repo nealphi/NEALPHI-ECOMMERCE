91b6c49f946c8a28e1e0bc5849211bf6b46a2239
Negin Alphi <negin.alipanahi@gmail.com>
2024-09-11 12:03:06 -0400

    Revise sameSite config

--- server/api/routes/user.ts
+++ server/api/routes/user.ts
@@ -90,7 +90,7 @@ router.post("/login", async (req: Request, res: Response) => {
     res.cookie("auth_token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production", // Secure only in production
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // "none" for production, "lax" for development
+      // sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // "none" for production, "lax" for development
       maxAge: 86400000,
     });
 
@@ -110,7 +110,7 @@ router.post("/logout", verifyToken, (req: Request, res: Response) => {
   res.cookie("auth_token", "", {
     httpOnly: true,  // Same as when you set the cookie
     secure: process.env.NODE_ENV === "production", // Use secure in production
-    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // "none" for production, "lax" for development
+    // sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // "none" for production, "lax" for development
     expires: new Date(0), // Set cookie to expire immediately
   });
   res.status(200).send({ message: "logged out" });
